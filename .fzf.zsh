650e813ad7d4a6a7a4e039f3b918124c52c9cf63
luoyebai <2112216825@qq.com>
2026-03-04 11:33:36 +0800

    [fix] update git

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/luoyebai/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/luoyebai/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 source <(fzf --zsh)
--- .zim/init.zsh
+++ .zim/init.zsh
@@ -1,4 +1,4 @@
-# FILE AUTOMATICALLY GENERATED FROM /home/luoyebai/.zimrc
+# FILE AUTOMATICALLY GENERATED FROM ${HOME}/.zimrc
 # EDIT THE SOURCE FILE AND THEN RUN zimfw build. DO NOT DIRECTLY EDIT THIS FILE!
 
 if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "${HOME}/.zim/zimfw.zsh" "${@}" }
